7c0c3b54168b4a62ec4de6c5f7d352ceba67a02f
Volodymyr Panov <vpanov25@gmail.com>
2024-04-16 17:18:32 +0200

    Solution

--- task.sh
+++ task.sh
@@ -1,1 +1,6 @@
 #! /bin/bash
+
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD"  ShopDB  --result-file=ShopDBReserve.sql
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD"  ShopDB  --no-create-info --result-file=ShopDBDevelopment.sql
+mysql -u "$DB_USER" -p"$DB_PASSWORD"  ShopDBReserve < ShopDBReserve.sql
+mysql -u "$DB_USER" -p"$DB_PASSWORD"  ShopDBDevelopment < ShopDBDevelopment.sql
